c1a399ae698674971c9151e5f6cf93749c9f1401
Jullianag <juh_g@hotmail.com>
2025-04-23 17:19:20 -0300

    Mandins captured pieces

--- src/main/java/org/meuprojeto/application/Program.java
+++ src/main/java/org/meuprojeto/application/Program.java
@@ -5,7 +5,9 @@ import org.meuprojeto.chess.ChessMatch;
 import org.meuprojeto.chess.ChessPiece;
 import org.meuprojeto.chess.ChessPosition;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 public class Program {
@@ -14,13 +16,14 @@ public class Program {
 
         Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
+        List<ChessPiece> captured = new ArrayList<ChessPiece>();
 
         while(true) {
 
             try {
 
                 UI.clearScreen();
-                UI.printMatch(chessMatch);
+                UI.printMatch(chessMatch, captured);
                 System.out.println();
                 System.out.print("Source: ");
                 ChessPosition source = UI.readChessPosition(sc);
@@ -36,6 +39,10 @@ public class Program {
 
                 System.out.println();
                 ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+
+                if (capturedPiece != null) {
+                    captured.add(capturedPiece);
+                }
             }
             catch (ChessException | InputMismatchException e) {
                 System.out.println(e.getMessage());
